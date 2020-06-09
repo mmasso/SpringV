e9c89f57be044d13934fe58112173ebc7beaeea6
mmasso <mmasso@cifpfbmoll.eu>
2020-06-09 15:12:00 +0200

    feat repositori crud repositori

--- src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -1,8 +1,13 @@
 package org.formacio.repositori;
 
+import org.formacio.domain.Factura;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
-public interface FacturesRepositori  {
+public interface FacturesRepositori extends CrudRepository<Factura, Long>  {
 
+	@Query("select count(factura.client) from Factura factura")
+	
 	public Number totalClient(String client);
 	
 }
