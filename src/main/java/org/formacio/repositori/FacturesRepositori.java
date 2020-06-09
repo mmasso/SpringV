ef1d822947e520c1652ff874aae9b9f09d9455ad
mmasso <mmasso@cifpfbmoll.eu>
2020-06-09 15:21:50 +0200

    feat test_total_client

--- src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -3,11 +3,11 @@ package org.formacio.repositori;
 import org.formacio.domain.Factura;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface FacturesRepositori extends CrudRepository<Factura, Long>  {
 
-	@Query("select count(factura.client) from Factura factura")
-	
-	public Number totalClient(String client);
+	@Query("select sum(linies.total) from Factura factura join factura.linies linies where factura.client.nom = :client")
+	public Number totalClient(@Param("client")String client);
 	
 }
