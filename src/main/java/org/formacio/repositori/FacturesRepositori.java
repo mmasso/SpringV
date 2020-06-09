a63f6af83a9fa4feb22203080e6702b562fd3476
mmasso <mmasso@cifpfbmoll.eu>
2020-06-09 15:27:29 +0200

    feat: test_query_generada

--- src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -1,13 +1,18 @@
 package org.formacio.repositori;
 
+import java.util.List;
+
 import org.formacio.domain.Factura;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+
+
 
 public interface FacturesRepositori extends CrudRepository<Factura, Long>  {
 
 	@Query("select sum(linies.total) from Factura factura join factura.linies linies where factura.client.nom = :client")
 	public Number totalClient(@Param("client")String client);
 	
+	public List<Factura> findByClientNom(String nom);
 }
